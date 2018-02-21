a206a508eec85e2d7abbec1d84824497c1094c80
ANKIT KUMAR <ankitkumar3305@gmail.com>
2018-02-21 23:18:35 +0530

    abstraction/customizeformcontrol/imp/davuniversity/mca

--- src/app/dashboard/scheduling/scheduling.component.ts
+++ src/app/dashboard/scheduling/scheduling.component.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -23,8 +23,8 @@ export class SchedulingComponent implements OnInit {
   user:  any = {
 
   }
-
-  constructor() {
+  reservedNumbers: any[] = ["12345", "9876", "987654321", "1234567890"]
+  constructor() {  
 
   }
 
@@ -46,9 +46,35 @@ export class SchedulingComponent implements OnInit {
         'username': new FormControl(user.userName,  Validators.required),
         'email' : new FormControl(user.emailId, [ Validators.required, Validators.email ]),
       }),
-      'phoneNumber' : new FormControl(user.phoneNumber, [ Validators.required ])
-      })
+      'hobbies': new FormArray([]),
+      'phoneNumber' : new FormControl(user.phoneNumber, [ Validators.required, this.validatePhoneNumber.bind(this) ])
+    })
   }
+
+  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  addHobbies(){
+    console.log("add hobbies called")
+    let inputFormControl = new FormControl("Test Hobbies", Validators.required);
+    (<FormArray>this.schedulingForm.get('hobbies')).push(inputFormControl)
+  }
+
+  validatePhoneNumber(control: FormControl) : {[s: string] : boolean}{
+    let value = control.value;
+    // if(value == 12345){
+    //   return { 'phoneNumberAvailability' : true};
+    // }
+
+    console.log(this.reservedNumbers)
+    console.log(value)
+    console.log("this.reservedNumbers.indexOf(value) "+this.reservedNumbers.indexOf(value))
+
+    if(this.reservedNumbers.indexOf(value) > -1){
+      return { 'phoneNumberAvailability' : true};
+    }
+
+    return null;
+  }
+
 
   submitForm(){
     console.log(this.schedulingForm)
